5cd0012d7cc15660fd83d59226e677ee60f33979
krayziejamaa <krayziejamaa@gmail.com>
2022-08-02 07:58:08 +0100

    updated script

--- javascript/script.js
+++ javascript/script.js
@@ -2,8 +2,8 @@ $(document).ready(function(){
     $('.dropdownCaret').on({
         'click': function() {
              var src = ($(this).attr('src') === '../images/icon-arrow-down.svg')
-                ? '../images/icon-arrow-up.svg'
-                : '../images/icon-arrow-down.svg';
+                ? './images/icon-arrow-up.svg'
+                : './images/icon-arrow-down.svg';
              $(this).attr('src', src);
         }
     });
